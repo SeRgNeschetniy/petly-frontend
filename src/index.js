5ba81664429287c7807b38c89c1f113f09366fc0
SeRgNeschetniy <xdsergx@gmail.com>
2023-01-28 13:52:09 +0200

    add theme

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
+import { Global, ThemeProvider } from '@emotion/react';
+import { GlobalStyles, theme } from 'styles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="petly-frontend">
-      <App />
+      <ThemeProvider theme={theme}>
+        <Global styles={GlobalStyles} />
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
---
+++ src/styles/theme.jsx
@@ -0,0 +1,23 @@
+export const theme = Object.freeze({
+  colors: {
+    accent: '#f59256',
+    white: '#ffffff',
+    black: '#000000',
+    dark: '#111111',
+    gray: 'rgba(17, 17, 17, 0.6)',
+    mainBackground: '#FDF7F2',
+  },
+  fontSizes: {
+    small: '12px',
+    medium: '16px',
+    large: '22px',
+  },
+  shadows: {
+    small: '0 5px 7px -1px rgba(51, 51, 51, 0.23)',
+    regular: '0px 4px 10px 4px #9e9e9e',
+    medium: '0 9px 47px 11px rgba(51, 51, 51, 0.18);',
+  },
+  animation: {
+    cubicBezier: '0.25s cubic-bezier(0.7, 0.98, 0.86, 0.98)',
+  },
+});
