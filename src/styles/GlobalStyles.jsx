694bc1d4a6a7806f1f4786d6e66237105296f2ac
SeRgNeschetniy <xdsergx@gmail.com>
2023-01-29 22:42:52 +0200

    add container

--- src/styles/GlobalStyles.jsx
+++ src/styles/GlobalStyles.jsx
@@ -71,4 +71,23 @@ export const GlobalStyles = css`
     font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
       monospace;
   }
+
+  .container {
+    padding: 0 20px;
+    margin: 0 auto;
+
+    @media screen and (min-width: 320px) {
+      width: 320px;
+    }
+
+    @media screen and (min-width: 768px) {
+      width: 768px;
+      padding: 0 32px;
+    }
+
+    @media screen and (min-width: 1280px) {
+      width: 1280px;
+      padding: 0 16px;
+    }
+  }
 `;
