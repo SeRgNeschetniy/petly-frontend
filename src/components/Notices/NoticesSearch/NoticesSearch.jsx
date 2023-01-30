a9fd6593705705eea9a4ac45a3fe8345381c350f
SeRgNeschetniy <xdsergx@gmail.com>
2023-01-30 15:40:43 +0200

    Update NoticesSearch.jsx

--- src/components/Notices/NoticesSearch/NoticesSearch.jsx
+++ src/components/Notices/NoticesSearch/NoticesSearch.jsx
@@ -1,37 +1,36 @@
-import { Form, Input, Button ,IconSearch} from "./NoticesSearch.styled";
+import { Form, Input, Button, IconSearch } from './NoticesSearch.styled';
 // import { useSelector, useDispatch } from "react-redux";
 // import { selectSearch } from "redux/search/search-selectors";
 // import { setSearch } from "redux/search/search-slice";
 
 const NoticesSearch = () => {
-    // const search = useSelector(selectSearch);
-    // const dispatch = useDispatch();
+  // const search = useSelector(selectSearch);
+  // const dispatch = useDispatch();
 
-    // const handleChange = e => setSearch(e.currentTarget.value.toLowerCase());  
+  // const handleChange = e => setSearch(e.currentTarget.value.toLowerCase());
 
-    // const handleSubmit = e => {
-    //     const { value } = e.target;
-    //     dispatch(setSearch(value));
-    // }
+  // const handleSubmit = e => {
+  //     const { value } = e.target;
+  //     dispatch(setSearch(value));
+  // }
 
-    
-    return (
-        <Form
-            // onSubmit={handleSubmit}
-        >
-                <Input
-                    type="text"
-                autoComplete="off"
-                autoFocus
-                placeholder="Search"
-                // onChange={handleChange}
-                // value={search}
-                />
-                <Button type="submit">
-                    <IconSearch size={24}  />
-                </Button>
-            </Form>
-    )
-}
+  return (
+    <Form
+    // onSubmit={handleSubmit}
+    >
+      <Input
+        type="text"
+        autoComplete="off"
+        autoFocus
+        placeholder="Search"
+        // onChange={handleChange}
+        // value={search}
+      />
+      <Button type="submit">
+        <IconSearch size={24} />
+      </Button>
+    </Form>
+  );
+};
 
 export default NoticesSearch;
