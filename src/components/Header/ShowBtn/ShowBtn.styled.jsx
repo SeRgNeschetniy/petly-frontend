07fab0df25378d5e3996342cd8c199b42f75103d
Lavelian <98335265+Lavelian@users.noreply.github.com>
2023-02-04 12:55:03 +0200

    fix bugs

--- src/components/Header/ShowBtn/ShowBtn.styled.jsx
+++ src/components/Header/ShowBtn/ShowBtn.styled.jsx
@@ -1,12 +1,20 @@
 import styled from '@emotion/styled';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { GrClose } from 'react-icons/gr';
 
+export const Burger = styled(GiHamburgerMenu)`
+  display: flex;
+  width: 30px;
+  height: 20px;
+`;
+export const Close = styled(GrClose)`
+  display: flex;
+  width: 20px;
+  height: 20px;
+`;
 export const Container = styled.button`
   margin-left: auto;
-  & > svg {
-    display: flex;
-    width: 20px;
-    height: 20px;
-  }
+
   @media screen and (min-width: 768px) and (max-width: 1280px) {
     margin-left: 25px;
   }
