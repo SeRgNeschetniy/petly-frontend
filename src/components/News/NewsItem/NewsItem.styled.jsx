61f1b4cc8e06a21a0a9b955a4e22ec716ced80b3
ArtemRe <RechembeiArtem@gmail.com>
2023-02-04 19:25:47 +0200

    verstka

--- src/components/News/NewsItem/NewsItem.styled.jsx
+++ src/components/News/NewsItem/NewsItem.styled.jsx
@@ -1,15 +1,15 @@
 import styled from 'styled-components';
-import { theme } from 'styles/theme';
+// import { theme } from 'styles/theme';
 
 export const Item = styled.li`
   padding: 12px 4px;
-//   background-color: red;
-  width: 391px;
-  height: 192px;
+
+  width: 280px;
+  height: 300px;
   margin-bottom: 60px;
 
   @media screen and (max-width: 767px) {
-    margin-bottom: 60px;
+  margin-bottom: 60px;
    width: 336px;
    height: 276px;
   }
@@ -25,13 +25,21 @@ export const Item = styled.li`
 `;
 export const Border = styled.div`
 
- border-top: 8px solid;
+ border-top: 4px solid;
 border-top-radius: 40px;
-border-top: 8px, solid;
+border-top: 4px, solid;
 border-image: linear-gradient(to right,#FF634E,#FFDF48) 1 0 0 0;
   
 
-
+}
+@media screen and (min-width: 768px){
+ border-top: 8px solid;
+border-top-radius: 40px;
+border-top: 8px, solid;
+}
+@media screen and (min-width: 1280px){
+  
+}
 
 `
 export const Heading = styled.h3`
@@ -39,23 +47,22 @@ font-family: 'Manrope';
 font-style: normal;
 font-weight: 700;
 font-size: 24px;
-line-height: 33px;
-letter-spacing: -0.01em;
+
 margin-top: 4px;
 /* Черный */
 
 color: #111111;
 overflow: hidden;
-    text-overflow: ellipsis;
-    -webkit-line-clamp: 2;
-    -webkit-box-orient: vertical;
-    display: -webkit-box;
-
+ text-overflow: ellipsis;
+-webkit-line-clamp: 2;
+-webkit-box-orient: vertical;
+display: -webkit-box;
 
   @media screen and (min-width: 768px) {
-    margin-bottom: 16px;
-    font-size: 16px;
-    line-height: 1.4;
+  margin-bottom: 16px;
+   font-weight: 700;
+   font-size: 24px;
+   line-height: 33px;
   }
 
   @media screen and (min-width: 1280px) {
@@ -79,6 +86,12 @@ overflow: hidden;
     -webkit-line-clamp: 5;
     -webkit-box-orient: vertical;
     display: -webkit-box;
+
+     @media screen and (min-width: 768px){
+
+     }
+@media screen and (min-width: 1280px){}
+
 `
 export const Box = styled.div`
     display: flex;
--- src/components/News/NewsList/NewsList.styled.jsx
+++ src/components/News/NewsList/NewsList.styled.jsx
@@ -1,17 +1,29 @@
 import styled from 'styled-components';
 
 export const List = styled.ul`
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(260px, 1fr));
-  grid-gap: 72px;
+  
+display: grid;
+  grid-template-columns: repeat(1, minmax(260px, 1fr));
+  // grid-gap: 40px 33px;
   margin-left: auto;
   margin-right: auto;
 
 
 
-
-
-   
+@media screen and (min-width: 768px) {
+display: grid;
+  grid-template-columns: repeat(2, minmax(260px, 1fr));
+  grid-gap: 60px 133px;
+  margin-left: auto;
+  margin-right: auto;
+}
+@media screen and (min-width: 1280px) {
+display: grid;
+  grid-template-columns: repeat(3, minmax(260px, 1fr));
+  grid-gap: 60px 33px;
+  margin-left: auto;
+  margin-right: auto;
+}
 `;
 
 
