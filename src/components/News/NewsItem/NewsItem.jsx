76c9e902645d9f06dfa9290eab519625a716b990
SeRgNeschetniy <xdsergx@gmail.com>
2023-02-05 23:47:04 +0200

    Update NewsItem.jsx

--- src/components/News/NewsItem/NewsItem.jsx
+++ src/components/News/NewsItem/NewsItem.jsx
@@ -1,28 +1,26 @@
-import React from "react";
-import {Heading, Wrapper, Box, Date, Item, Link, Border, } from './NewsItem.styled'
+import React from 'react';
+import {
+  Heading,
+  Wrapper,
+  Box,
+  Date,
+  Item,
+  Link,
+  Border,
+} from './NewsItem.styled';
 
-
-const NewsItem = ({
-  title,
-  url,
-  description,
-  date,
-  
-}) => {
+const NewsItem = ({ title, url, description, date }) => {
   return (
-      <Item>
-          <Border>
-              
-          </Border>
-          <Heading>{title}</Heading>
-          <Wrapper>{description}</Wrapper>
-          <Box>
-              <Date>{date}</Date>
-              <a href={url} target="_blank" rel="noopener noreferrer"> <Link>Read more</Link>
-                  
-              </a>
-          </Box>
-          
+    <Item>
+      <Border></Border>
+      <Heading>{title}</Heading>
+      <Wrapper>{description}</Wrapper>
+      <Box>
+        <Date>{date}</Date>
+        <Link href={url} target="_blank" rel="noopener noreferrer">
+          Read more
+        </Link>
+      </Box>
     </Item>
   );
 };
