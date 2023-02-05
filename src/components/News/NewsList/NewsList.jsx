813291672bbb179cdb46cf1e621e31b7e927ed4a
SeRgNeschetniy <xdsergx@gmail.com>
2023-02-05 23:27:37 +0200

    bugifx

--- src/components/News/NewsList/NewsList.jsx
+++ src/components/News/NewsList/NewsList.jsx
@@ -2,15 +2,12 @@ import React from 'react';
 import NewsItem from '../NewsItem/NewsItem';
 import { List } from './NewsList.styled';
 
-
-  
-const NewsList = ({ News }) => {
+const NewsList = ({ News = [] }) => {
   if (News.length < 1) {
     return <p>Something went wrong...</p>;
   }
   return (
     <List>
-      
       {News.map(item => {
         return <NewsItem key={item._id} {...item} />;
       })}
