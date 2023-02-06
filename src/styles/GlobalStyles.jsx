439772de63b0c6cd7755bba88b88d16ae17f507a
Lavelian <98335265+Lavelian@users.noreply.github.com>
2023-02-07 01:11:26 +0200

    fix

--- src/styles/GlobalStyles.jsx
+++ src/styles/GlobalStyles.jsx
@@ -101,17 +101,13 @@ export const Container = styled.div`
   padding: 0 20px;
   margin: 0 auto;
 
-  @media screen and (min-width: 320px) {
-    width: 320px;
-  }
+  min-width: 320px;
 
   @media screen and (min-width: 768px) {
-    width: 768px;
     padding: 0 32px;
   }
 
   @media screen and (min-width: 1280px) {
-    width: 1280px;
     padding: 0 16px;
   }
 `;
