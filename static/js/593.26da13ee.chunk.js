0bd401d9d92242ad4857233b95912947242da345
SeRgNeschetniy <52968351+SeRgNeschetniy@users.noreply.github.com>
2023-02-06 23:14:45 +0000

    Deploying to gh-pages from @ SeRgNeschetniy/petly-frontend@456271c2a91c558d6f146e735786d76c3b09d2d2 🚀

---
+++ static/js/593.26da13ee.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[593],{593:function(e,t,n){n.r(t);var o=n(1743),r=n(4128),s=n(9434),l=n(8225),c=n(184);t.default=function(){var e=(0,s.v9)(l.xU),t=(0,s.v9)(l.lB);return console.log("Render NoticesOwn",t),(0,c.jsxs)(c.Fragment,{children:[0===(null===t||void 0===t?void 0:t.length)&&!e&&(0,c.jsx)("p",{children:"List is empty! Try to add pet :)"}),e&&(0,c.jsx)(o.Z,{}),!e&&(0,c.jsx)(r.Z,{route:"own",notices:t})]})}}}]);
+//# sourceMappingURL=593.26da13ee.chunk.js.map
