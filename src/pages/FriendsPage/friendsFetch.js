980719980b4e1a8b64061ef69da4680b818b2a17
Yuliia-Harkusha <97845497+Yuliia-Harkusha@users.noreply.github.com>
2023-02-06 21:02:43 +0100

    update

--- src/pages/FriendsPage/friendsFetch.js
+++ src/pages/FriendsPage/friendsFetch.js
@@ -1,7 +1,7 @@
 const { REACT_APP_BASE_URL } = process.env;
 
 const fetchFriends = async () => {
-  const response = await fetch(`${REACT_APP_BASE_URL}api/friends`);
+  const response = await fetch(`${REACT_APP_BASE_URL}/api/friends`);
   return await response.json();
 };
 
