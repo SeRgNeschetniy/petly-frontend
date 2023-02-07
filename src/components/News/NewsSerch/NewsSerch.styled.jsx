f8d14cb59e1491cceb3c0828de8be24f743900fe
SeRgNeschetniy <xdsergx@gmail.com>
2023-02-07 13:28:41 +0200

    Update NewsSerch.styled.jsx

--- src/components/News/NewsSerch/NewsSerch.styled.jsx
+++ src/components/News/NewsSerch/NewsSerch.styled.jsx
@@ -3,26 +3,23 @@ import { theme } from 'styles/theme';
 import { AiOutlineSearch } from 'react-icons/ai';
 
 export const Form = styled.form`
-margin-bottom:28px;
+  margin-bottom: 28px;
 
-position:relative;
-display: flex;
-justify-content: center;
-// padding-bottom:40px;
-border:transparent;
-background: ${theme.colors.white};
-box-shadow: 7px 4px 14px rgba(49, 21, 4, 0.07);
-border-radius: 40px;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  // padding-bottom:40px;
+  border: transparent;
+  background: ${theme.colors.white};
+  box-shadow: 7px 4px 14px rgba(49, 21, 4, 0.07);
+  border-radius: 40px;
 
-@media screen and (min-width: 768px) {
-    margin-bottom:40px;
-    width:608px;
+  @media screen and (min-width: 768px) {
+    margin-bottom: 40px;
+    width: 608px;
     margin-right: auto;
     margin-left: auto;
-}
-
   }
-
 `;
 
 export const Input = styled.input`
