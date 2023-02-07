6ce948fa273e57bfa358a9e071058ec4e5b5d801
SeRgNeschetniy <xdsergx@gmail.com>
2023-02-07 12:16:29 +0200

    Update GoogleIcon.jsx

--- src/components/Auth/GoogleSignIn/GoogleIcon.jsx
+++ src/components/Auth/GoogleSignIn/GoogleIcon.jsx
@@ -1,11 +1,16 @@
-import { IconWrapper, IconLink, GoogleIconStyle } from "./GoogleIcon.styled";
+import { IconWrapper, IconLink, GoogleIconStyle } from './GoogleIcon.styled';
+
+const { REACT_APP_BASE_URL } = process.env;
 
 export default function GoogleIcon() {
   return (
     <IconWrapper>
-      <IconLink href="https://petly-backend-vopf.onrender.com/api/users/google" target="_self">
-        <GoogleIconStyle src={require('images/auth/google.png')} alt="google-sign-in"/>
+      <IconLink href={`${REACT_APP_BASE_URL}/users/google`} target="_self">
+        <GoogleIconStyle
+          src={require('images/auth/google.png')}
+          alt="google-sign-in"
+        />
       </IconLink>
     </IconWrapper>
-  )
+  );
 }
