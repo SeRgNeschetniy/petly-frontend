1edb4ac4c5b286841e8620e8da872312791bb800
Yuliia-Harkusha <97845497+Yuliia-Harkusha@users.noreply.github.com>
2023-02-07 12:53:15 +0100

    fix styles

--- src/components/Friends/FriendsItem/WorkHours.jsx
+++ src/components/Friends/FriendsItem/WorkHours.jsx
@@ -19,23 +19,9 @@ const WorkHours = ({ workDays }) => {
     setShowModal(!showModal);
   };
 
-  //   let time = '';
-  //   const openDay = workDays.find(item => item.isOpen);
-  //   if (openDay) {
-  //     time = `${workDays[updateIdx()]?.from}-${workDays[updateIdx()]?.to}`;
-  //   }
-  //   const closeDay = workDays.find(item => item.isOpen === false);
-  //   if (closeDay) {
-  //     time = 'Closed';
-  //   }
-
   return (
     <>
       <TimeBtn type="button" onClick={handleClick}>
-        {/* {workDays?.length > 0
-          ? `${workDays[updateIdx()]?.from}-${workDays[updateIdx()]?.to}`
-          : 'Closed'} */}
-
         {workDays[updateIdx()].from && workDays[updateIdx()].to
           ? `${workDays[updateIdx()]?.from} - ${workDays[updateIdx()]?.to}`
           : 'Closed'}
