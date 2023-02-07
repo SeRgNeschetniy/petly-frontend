23eac73881067d63e1ec6c5ce9124b9365d10b74
Nikolay Chereliuk <kolyanerushaev@gmail.com>
2023-02-07 19:16:07 +0200

    Update UserDataForm

--- src/components/UserPage/UserData/UserInput/UserCity/UserCity.jsx
+++ src/components/UserPage/UserData/UserInput/UserCity/UserCity.jsx
@@ -2,20 +2,32 @@ import { BoxInput,TextField,InputForm,ButtonEdit } from "./UserCity.styled"
 import { FiEdit2, FiCheck } from 'react-icons/fi';
 import { useState } from "react";
 import { selectUser } from "redux/auth/auth-selectors";   
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { patchContact } from "redux/userpage/userpage-operation";
 
 export default function UserInputCity() {
-const user = useSelector(selectUser) ;
-    
+    const user = useSelector(selectUser) ;
+    const [city, setCity] = useState(user.city);
+    const dispatch = useDispatch();
     const [disabled, setDisabled] = useState(true);
 
-    function handleGameClick() {
-        setDisabled(!disabled);
+    const handleChange = (e) => {
+    setCity(e.target.value);
+}
+
+    function handleGameClick(e) {
+    if (disabled) {
+    setDisabled(false);
+    } else {
+      dispatch(patchContact({city: city}));
+      setDisabled(true);
     }
+    
+  }
     return(
     <BoxInput>
         <TextField>City:</TextField>
-        <InputForm value={user.city} type="text" disabled={ disabled} />
+        <InputForm onChange={handleChange}  value={city}  type="text" disabled={ disabled} />
             <ButtonEdit type='submit' onClick={handleGameClick}  >
             <div>{disabled ? <FiEdit2 color="#F59256"/> : <FiCheck color="#F59256"/>}</div>
         </ButtonEdit>
--- src/components/UserPage/UserData/UserInput/UserPhone/UserPhone.jsx
+++ src/components/UserPage/UserData/UserInput/UserPhone/UserPhone.jsx
@@ -4,19 +4,33 @@ import { FiEdit2, FiCheck } from 'react-icons/fi';
 import { useState } from "react";
 import { useSelector } from "react-redux";
 import { selectUser } from "redux/auth/auth-selectors";
-    
-export default function UserInputPhone() {
-   
-const user = useSelector(selectUser) ;
- const [disabled, setDisabled] = useState(true);
+import { useDispatch } from "react-redux";
+import { patchContact } from "redux/userpage/userpage-operation";    
 
-  function handleGameClick() {
-    setDisabled(!disabled);
+
+export default function UserInputPhone() {
+    const user = useSelector(selectUser) ;
+    const [phone, setName] = useState(user.phone);
+    const dispatch = useDispatch();
+    const [disabled, setDisabled] = useState(true);
+
+    const handleChange = (e) => {
+    setName(e.target.value);
+}
+
+    function handleGameClick(e) {
+    if (disabled) {
+    setDisabled(false);
+    } else {
+      dispatch(patchContact({phone: phone}));
+      setDisabled(true);
+    }
+    
   }
 return(
     <BoxInput>
         <TextField>Phone:</TextField>
-        <InputForm value={user.phone} type="text" disabled={ disabled} />
+        <InputForm onChange={handleChange} value={phone} type="text" disabled={ disabled} />
         <ButtonEdit type='submit' onClick={handleGameClick} >
             <div>{disabled ? <FiEdit2 color="#F59256"/> : <FiCheck color="#F59256"/>}</div>
         </ButtonEdit>
