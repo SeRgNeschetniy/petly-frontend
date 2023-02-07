48b4bdd98fd03fb555d249559e083591d00c6fb6
SeRgNeschetniy <xdsergx@gmail.com>
2023-02-07 13:05:25 +0200

    bugfix

--- src/components/PrivateRoute/PrivateRoute.js
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -2,8 +2,9 @@ import { useAuth } from 'hooks/useAuth';
 import { Navigate } from 'react-router-dom';
 
 const PrivateRoute = ({ component: Component, redirectTo = '/' }) => {
-  const { token } = useAuth();
-  return token ? <Component /> : <Navigate to={redirectTo} replace />;
+  const { isLoggedIn, isRefreshing } = useAuth();
+  const redirect = !isLoggedIn && !isRefreshing;
+  return redirect ? <Navigate to={redirectTo} replace /> : <Component />;
 };
 
 export default PrivateRoute;
