5ccbd4dd63f19b39faffeb9b59272a5ace6a3622
SeRgNeschetniy <xdsergx@gmail.com>
2023-02-08 04:39:01 +0200

    Update UserPage.styled.jsx

--- src/pages/UserPage/UserPage.styled.jsx
+++ src/pages/UserPage/UserPage.styled.jsx
@@ -1,12 +1,9 @@
 import styled from '@emotion/styled';
 
-
-
-
 export const Container = styled.div`
-  display:flex;
+  display: flex;
   background-color: ${({ theme }) => theme.mainBackground};
-  
+
   flex-direction: column;
   margin: 0 auto;
 
@@ -16,22 +13,19 @@ export const Container = styled.div`
 
   @media screen and (min-width: 768px) {
     width: 768px;
-    
   }
 
   @media screen and (min-width: 1280px) {
     width: 1280px;
     padding: 0 16px;
-    display:flex;
-   flex-direction: row
+    display: flex;
+    flex-direction: row;
   }
-  
 `;
 
 export const UserPetsData = styled.div`
-
-@media screen and (min-width: 1280px) {
-display:flex;
-}
-`
-
+  @media screen and (min-width: 1280px) {
+    justify-content: space-between;
+    display: flex;
+  }
+`;
