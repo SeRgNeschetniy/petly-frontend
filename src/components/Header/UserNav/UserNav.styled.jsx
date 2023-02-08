f91d9f9a4b16aa58c65a998b60d34fd4d8fd27c2
Ohleh <oleg.kolischuk@gmail.com>
2023-02-08 12:41:42 +0200

    pain border account

--- src/components/Header/UserNav/UserNav.styled.jsx
+++ src/components/Header/UserNav/UserNav.styled.jsx
@@ -26,6 +26,7 @@ export const UserLinks = styled(NavLink)`
   &:hover,
   &:focus {
     background-color: #ff6101;
+    border: 2px solid #ff6101;
   }
 
   transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
