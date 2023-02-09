f679d0e7358f717a797168d4ffe803b6232a6efb
SeRgNeschetniy <xdsergx@gmail.com>
2023-02-09 14:16:38 +0200

    Update NoticesOwn.jsx

--- src/pages/NoticesPage/NoticesOwn.jsx
+++ src/pages/NoticesPage/NoticesOwn.jsx
@@ -2,15 +2,23 @@ import Loader from 'components/Loader';
 import NoticesCategoriesList from 'components/Notices/NoticesCategoriesList/NoticesCategoriesList';
 import EmpyList from 'components/Notices/EmpyList/EmptyList';
 import { LazyLoadComponent } from 'react-lazy-load-image-component';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 import {
   selectIsLoading,
   selectUserNoitices,
 } from 'redux/notices/notices-selectors';
+import { fetchUserNotices } from 'redux/notices/notices-operation';
 
 const NoticesOwn = () => {
   const isLoading = useSelector(selectIsLoading);
   const notices = useSelector(selectUserNoitices);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUserNotices());
+  }, [dispatch]);
 
   return (
     <>
