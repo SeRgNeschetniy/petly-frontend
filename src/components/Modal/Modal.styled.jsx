6e3438feb5f1d8ef6141874e4f85b127fd5b32cd
SeRgNeschetniy <xdsergx@gmail.com>
2023-02-10 10:25:57 +0200

    Update Modal.styled.jsx

--- src/components/Modal/Modal.styled.jsx
+++ src/components/Modal/Modal.styled.jsx
@@ -6,7 +6,7 @@ export const Overlay = styled.div`
   height: 100%;
 
   display: flex;
-  align-items: flex-start;
+  align-items: center;
   justify-content: center;
   background-color: rgba(0, 0, 0, 0.75);
   overflow: hidden;
